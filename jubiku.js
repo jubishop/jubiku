c84d1141c203e311598e0721857c04dcd4e0b45d
Justin Bishop <jubishop@gmail.com>
2016-01-01 15:43:16 -0800

    canvas crap

--- jubiku.js
+++ jubiku.js
@@ -1,7 +1,17 @@
-var Jubiku = function (canvasDiv, squares, rowHints, columnHints, internalNumbers) {
-    console.log(canvasDiv);
+var JubiSquare = function (pen, shouldExist, row, column) {
+    pen.fillRect(0, 0, 100, 100);
+}
+
+var Jubiku = function (canvas, squares, rowHints, columnHints, internalNumbers) {
+    console.log(canvas);
     console.log(squares);
     console.log(rowHints);
     console.log(columnHints);
     console.log(internalNumbers);
+    
+    var pen = canvas.getContext('2d');
+    pen.fillStyle = 'green';
+
+    new JubiSquare(pen, true, 0, 0);
 }
+
