5091788ffdd51553bf097c90b160684c9082d811
Justin Bishop <jubishop@gmail.com>
2016-01-01 17:16:25 -0800

    actually painting grid of squares now

--- jubiku.js
+++ jubiku.js
@@ -1,17 +1,20 @@
-var JubiSquare = function (pen, shouldExist, row, column) {
-    pen.fillRect(0, 0, 100, 100);
+class JubiSquare {
+    constructor(pen, shouldExist, row, column) {
+        pen.fillRect(110 * row , 110 * column, 100, 100);
+    }
 }
 
-var Jubiku = function (canvas, squares, rowHints, columnHints, internalNumbers) {
-    console.log(canvas);
-    console.log(squares);
-    console.log(rowHints);
-    console.log(columnHints);
-    console.log(internalNumbers);
-    
-    var pen = canvas.getContext('2d');
-    pen.fillStyle = 'green';
+class Jubiku {
+    constructor(canvas, squares, rowHints, columnHints, internalNumbers) {
+        var pen = canvas.getContext('2d');
+        pen.fillStyle = 'green';
 
-    new JubiSquare(pen, true, 0, 0);
+        this.jubiSquares = squares.map(function(rowElement, row, _array) {
+            return rowElement.map(function(columnValue, column, _array) {
+                return new JubiSquare(pen, columnValue, row, column)
+            });
+        });
+        console.log(this.jubiSquares);
+    }
 }
 
